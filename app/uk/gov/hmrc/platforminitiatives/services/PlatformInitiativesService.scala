e3daede34f735bba18dff38f36883fcd80330926
Shnick <1132919+Shnick@users.noreply.github.com>
2023-01-04 11:42:39 +0000

    BDOG-2333 change Java 11 Upgrade description

--- app/uk/gov/hmrc/platforminitiatives/services/PlatformInitiativesService.scala
+++ app/uk/gov/hmrc/platforminitiatives/services/PlatformInitiativesService.scala
@@ -103,7 +103,7 @@ class PlatformInitiativesService @Inject()(
       ),
       createJavaInitiative(
         initiativeName        = "Java 11 Upgrade",
-        initiativeDescription = s"""Services should be built and run with Java 11 before defaults are changed - [JDK Versions: latest](${url"https://catalogue.tax.service.gov.uk/jdkexplorer/latest?team=$teamName"}) | [Confluence](${url"https://confluence.tools.tax.service.gov.uk/x/TwLIH"})""",
+        initiativeDescription = s"""[Java 11 upgrade](${url"https://catalogue.tax.service.gov.uk/jdkexplorer/latest?team=$teamName"}) | [Confluence](${url"https://confluence.tools.tax.service.gov.uk/x/TwLIH"})""",
         version               = Version.apply("11.0.0"),
         team                  = team
       )
