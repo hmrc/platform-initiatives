3e887015d4bbe67a0ad854179899f4282078e60e
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-04-17 15:51:06 +0100

    drop unused mongo dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,19 +5,16 @@ import sbt._
 
 object AppDependencies {
   val bootstrapPlayVersion = "8.5.0"
-  val hmrcMongoVersion     = "1.7.0"
 
   val compile = Seq(
     ws,
     ehcache,
-    "uk.gov.hmrc"           %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-metrix-play-30" % hmrcMongoVersion,
-    "org.typelevel"         %% "cats-core"                 % "2.10.0"
+    "uk.gov.hmrc"    %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
+    "org.typelevel"  %% "cats-core"                 % "2.10.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapPlayVersion  % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % hmrcMongoVersion      % Test,
-    "org.mockito"            %% "mockito-scala-scalatest"     % "1.17.29"             % Test
+    "uk.gov.hmrc"    %% "bootstrap-test-play-30"   % bootstrapPlayVersion  % Test,
+    "org.mockito"    %% "mockito-scala-scalatest"  % "1.17.29"             % Test
   )
 }
