6fe943325bf9447cee88efe8110814de0aa464f7
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-11-28 16:38:04 +0000

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,14 @@
-import play.sbt.PlayImport.{ehcache, ws}
-
 import sbt._
 
-
 object AppDependencies {
-  val bootstrapPlayVersion = "9.0.0"
+  val bootstrapPlayVersion = "9.5.0"
 
   val compile = Seq(
-    ws,
-    ehcache,
     "uk.gov.hmrc"    %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
-    "org.typelevel"  %% "cats-core"                 % "2.10.0"
+    "org.typelevel"  %% "cats-core"                 % "2.12.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapPlayVersion  % Test,
-    "org.scalatestplus" %% "mockito-3-4"            % "3.2.10.0"            % Test
+    "uk.gov.hmrc"    %% "bootstrap-test-play-30"    % bootstrapPlayVersion  % Test
   )
 }
